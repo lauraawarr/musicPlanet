67c36448e19c1d0482e0bdb3c01a886ec45857a1
Laura Warr <laurawarr@Lauras-MacBook-Pro.local>
2016-12-14 10:55:50 -0500

    Updated comment inserting to avoid duplicates

--- search.singer.php
+++ search.singer.php
@@ -1,17 +1,12 @@
 <?php
-
 $db = new PDO('mysql:host=localhost;dbname=musicPlanet;charset=utf8','root','root');
 $db -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 $db -> setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-
 //if no singer is set, redirect to search page
 if( !isset($_GET['searchID']) ) {
-
 	header('Location: search.php');
-
 } else {
 	$searchID = $_GET['searchID'];
-
 	$sql = "SELECT * FROM singers
 			INNER JOIN comments
 			ON (singers.singer_id = comments.singer_id) 
@@ -20,23 +15,19 @@ if( !isset($_GET['searchID']) ) {
 	//$query->bindParam( ":twitter" , $_SESSION['twitter'] );
 	$query -> execute( array(':id' => $searchID ));
 	$searchComments = $query->fetchAll(PDO::FETCH_ASSOC);
-
 	if (empty($searchComments)){
 		$sql = "SELECT * FROM singers WHERE singer_id = :id";
 		$query = $db->prepare( $sql );
 		//$query->bindParam( ":twitter" , $_SESSION['twitter'] );
 		$query -> execute( array(':id' => $searchID ));
 		$searchSinger = $query->fetch(PDO::FETCH_ASSOC);
-
 		$searchComments = [];
 		$obj = ['comment' => 'No comments yet...'];
 		array_push($searchComments, $obj);
-
 	} else {
 		$searchSinger = $searchComments[0];
 	}; //end if searchComments empty
 }; //end if searchID set
-
 ?>
 
 <!DOCTYPE html>
@@ -65,36 +56,44 @@ if( !isset($_GET['searchID']) ) {
 	<?php 
 		// This inserts user input into database 
 		if ($_SERVER["REQUEST_METHOD"] == "POST"){
+			// Checks if comment is already in databse
 			if(!empty($_POST['newComment'])){
 
 				$newComment = $_POST['newComment'];
-				$_POST['newComment'] = ''; //sets POST to be empty after declaring variable
-
-				$sql = "INSERT INTO comments (singer_id, comment) VALUES (:searchID, :newComment)";
+				$sql = "SELECT * FROM comments
+						WHERE comment = :newComment
+						AND singer_id = :singer_id";
 				$query = $db->prepare( $sql );
-				//$query->bindParam( ":twitter" , $_SESSION['twitter'] );
-				$query -> execute([':searchID' => $searchID, ':newComment' => $newComment]);
+				$query -> execute( array(':newComment' => $newComment, ':singer_id'=> $searchSinger['singer_id']));
+				$duplicate = $query->fetchAll(PDO::FETCH_ASSOC);
+				
+				// Does not insert into table if duplicate
+				if (empty($duplicate)){
 
+					$sql = "INSERT INTO comments (singer_id, comment) VALUES (:searchID, :newComment)";
+					$query = $db->prepare( $sql );
+					$query -> execute([':searchID' => $searchID, ':newComment' => $newComment]);
+
+				}; // end duplicate
 			}; //end isset newComment
 		};
-
 		// Prints all comments about singer ($searchComments is array of comments about $searchSinger)
 			// $searchSinger['singer_name'] is singer's name
 			// each row in $searchComments has attributes: singer_id, comment
 		
-		echo "<div class = 'img'>"."<img height='300px' src='_images/_singers/".$searchSinger["image"]."'/>"."<h1>".$searchSinger['singer_name']."</h1>"."</div>";
+		echo "<div class = 'img'>"."<img height='400px' src='_images/_singers/".$searchSinger["image"]."'/>"."</div>";
+		echo "<h1>".$searchSinger['singer_name']."</h1>";
 		echo "<div class='comment'>";
 		foreach ($searchComments as $row){
 			echo "<p>".$row['comment']."</p>";
 		};
 		echo "</div>";
-
 	?>
 
 	<!-- Takes in user input comments -->
 	<div class="form">
 	<form method='POST' action='search.singer.php?searchID=<?php echo $searchSinger["singer_id"]?>'>
-		<input name='newComment' type='text' placeholder="Add your comment here..."></input>
+		<input name='newComment' type='text'></input>
 		<input name='commentSubmit' type='submit' value='Post' class='submit' required />
 	</form>
 	</div>
