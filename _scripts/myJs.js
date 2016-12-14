0443d2911678c3408e6fc7b14e027a91174f5244
Laura Warr <laurawarr@Lauras-MacBook-Pro.local>
2016-12-14 11:21:26 -0500

    Updated script to prevent comments from overlapping with singer profile

--- _scripts/myJs.js
+++ _scripts/myJs.js
@@ -1,9 +1,17 @@
+function getRandomPosition(element) {
+  var x = document.body.offsetWidth-element.clientWidth;
+  var y = document.body.offsetHeight-element.clientHeight;
 
- function getRandomPosition(element) {
-  var x = document.body.offsetHeight-element.clientHeight;
-  var y = document.body.offsetWidth-element.clientWidth;
-  var randomX = Math.floor(Math.random()*x);
+  var img = document.querySelector('.img');
+  var imgW = img.clientWidth;
+
+  var newX = x - imgW -element.clientWidth;
+  var posX = img.getBoundingClientRect().left;
+  var tempX = Math.floor(Math.random()*newX);
+
+  var randomX = (tempX >= posX-element.clientWidth) ? tempX + imgW + element.clientWidth: tempX;
   var randomY = Math.floor(Math.random()*y);
+
   return [randomX,randomY];
 }
 
@@ -11,8 +19,9 @@ window.onload = function() {
 
   var p = document.querySelectorAll('p');
   for(var i=0; i<p.length; i++){
-  var xy = getRandomPosition(p[i]);
-  p[i].style.top = xy[0] + 'px';
-  p[i].style.left = xy[1] + 'px';
+    var xy = getRandomPosition(p[i]);
+    p[i].style.left = xy[0] + 'px';
+    p[i].style.top = xy[1] + 'px';
+  };
+
 };
-}
