922667c698d41ed6bc90491f39769aaa00c2974d
Laura Warr <laurawarr@Lauras-MacBook-Pro.local>
2016-12-16 11:33:48 -0500

    Cleaned up files

--- search.php
+++ search.php
@@ -1,6 +1,6 @@
 <?php 
 	$db = new PDO('mysql:host=localhost;dbname=warrla_musicPlanet;charset=utf8','warrla_user','musicPlanetUser123');
-	//$db = new PDO('mysql:host=localhost;dbname=musicPlanet;charset=utf8','root','root');
+	// $db = new PDO('mysql:host=localhost;dbname=musicPlanet;charset=utf8','root','root');
 	//set error mode, which allows errors to be thrown, rather than silently ignored
 	$db -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 	$db -> setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
